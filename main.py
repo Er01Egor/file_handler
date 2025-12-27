af8ff8d6886d07e22b1fac763d7fc757140dbd08
erlyg <erlyginegor@gmail.com>
2025-12-27 16:37:23 +0300

    начало текстового редаткора на Qt (добавлен ui файл с кнопками и вводами)

--- main.py
+++ main.py
@@ -1,47 +1,139 @@
 import sys
+import io
 
-try:
-    print('Выберите действие: 1) создать файл; 2) открыть существующий')
-    input_ = int(input('Выберите число, соответсвующее вашему выбору (1 или 2): '))
-    if input_ == 2:
-        try:
-            file_name = input('Введите имя файла: ')
-            # text = input('Введите текст: ')
-            print(f'Что сделать с файлом "{file_name}"')
-            print('1) прочитать; 2) изменить')
-            input_num = int(input('Выберите число, соответсвующее вашему выбору (1 или 2): '))
-            if input_num == 1:
-                content_file = []
-                with open(file_name, 'r', encoding='utf-8') as file:
-                    print(f'Содержание файла "{file_name}":')
-                    content_file.append('--------- Начало файла ----------')
-                    content_file.append(file.read())
-                    content_file.append('--------- Конец файла ----------')
-                for line in content_file:
-                    print(line)
-            if input_num == 2:
-                print(f'Что записать в файл "{file_name}"')
-                print('Введите текст (нажмите CTRL + D что бы остановить ввод): ')
-                content_file = list(map(str, sys.stdin))
-                with open(file_name, 'a', encoding='utf-8') as file:
-                    for line in content_file:
-                        result = line.strip('\n')
-                        val = file.write(result + '\n')
-        except FileNotFoundError:
-            print(f'Файл с именем: "{file_name}" отсутствовал')
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QWidget, QFileDialog
 
-    if input_ == 1:
-        file_name = input('Введите имя нового файла: ')
+template = """<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>Form</class>
+ <widget class="QWidget" name="Form">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>833</width>
+    <height>676</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Form</string>
+  </property>
+  <widget class="QPushButton" name="create_file_button">
+   <property name="geometry">
+    <rect>
+     <x>520</x>
+     <y>40</y>
+     <width>181</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Создать файл</string>
+   </property>
+  </widget>
+  <widget class="QPushButton" name="open_file_button">
+   <property name="geometry">
+    <rect>
+     <x>520</x>
+     <y>110</y>
+     <width>181</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Открыть существующий</string>
+   </property>
+  </widget>
+  <widget class="QPushButton" name="read_file_button">
+   <property name="geometry">
+    <rect>
+     <x>70</x>
+     <y>170</y>
+     <width>111</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Прочитать</string>
+   </property>
+  </widget>
+  <widget class="QPushButton" name="correct_file_button">
+   <property name="geometry">
+    <rect>
+     <x>210</x>
+     <y>170</y>
+     <width>111</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Изменить</string>
+   </property>
+  </widget>
+  <widget class="QLineEdit" name="file_names">
+   <property name="geometry">
+    <rect>
+     <x>50</x>
+     <y>70</y>
+     <width>371</width>
+     <height>31</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QTextEdit" name="file_content">
+   <property name="geometry">
+    <rect>
+     <x>20</x>
+     <y>270</y>
+     <width>801</width>
+     <height>371</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QPushButton" name="save_button">
+   <property name="geometry">
+    <rect>
+     <x>490</x>
+     <y>210</y>
+     <width>111</width>
+     <height>28</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Сохранить</string>
+   </property>
+  </widget>
+  <widget class="QPushButton" name="delete_button">
+   <property name="geometry">
+    <rect>
+     <x>630</x>
+     <y>210</y>
+     <width>111</width>
+     <height>28</height>
+    </rect>
+   </property>
+   <property name="text">
+    <string>Удалить </string>
+   </property>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
 
-        print(f'Что записать в файл "{file_name}"')
-        print('Введите текст (нажмите CTRL + D что бы остановить ввод): ')
-        content_file = list(map(str, sys.stdin))
-        with open(file_name, 'w', encoding='utf-8') as file:
-            # content_file = content_file
-            for line in content_file:
-                result = line.strip('\n')
-                val = file.write(result + '\n')
+"""
 
 
-except Exception as err:
-    print(f'Произошла ошибка: {err}')
+class FileHandler(QWidget):
+    def __init__(self):
+        super().__init__()
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = FileHandler()
+    window.show()
+    sys.exit(app.exec())
