4735f055e57583dd7a52aede91cec01a84ee43f8
erlyg <erlyginegor@gmail.com>
2025-12-27 12:48:27 +0300

    добавлена функция создания нового файла

--- main.py
+++ main.py
@@ -23,5 +23,18 @@ try:
         except FileNotFoundError:
             print(f'Файл с именем: "{file_name}" отсутствовал')
 
+    if input_ == 1:
+        file_name = input('Введите имя нового файла: ')
+
+        print(f'Что записать в файл "{file_name}"')
+        print('Введите текст (нажмите CTRL + D что бы остановить ввод): ')
+        content_file = list(map(str, sys.stdin))
+        with open(file_name, 'w', encoding='utf-8') as file:
+            # content_file = content_file
+            for line in content_file:
+                result = line.strip('\n')
+                val = file.write(result + '\n')
+
+
 except Exception as err:
     print(f'Произошла ошибка: {err}')
